a28101617b3a77889a2f55d88084e6d7547dea2b
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-08-30 01:00:27 +0900

    講義ページの学群画像を追加

--- review/views.py
+++ review/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 class IndexView(TemplateView):
     template_name = 'review/index.html'
+
+class LectureView(TemplateView):
+    template_name = 'review/lecture.html'
