5d2d96dbce28c164904863d58db8763328673ad7
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-09-03 01:50:04 +0900

    モーダルで投稿できるようにした

--- review/views.py
+++ review/views.py
@@ -1,7 +1,17 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
 
 class IndexView(TemplateView):
     template_name = 'review/index.html'
 
 class LectureView(TemplateView):
+    http_method_names = ['get', 'post']
     template_name = 'review/lecture.html'
+
+    def post(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['overall'] = request.POST['overall']
+        context['difficulty'] = request.POST['difficulty']
+        context['kadai'] = request.POST['kadai']
+        context['evaluation'] = request.POST['evaluation']
+        return render(request, 'review/lecture.html', context=context) 
---
+++ static/js/common.js
@@ -0,0 +1,4 @@
+// モーダルで投稿するための関数
+function form_submit() {
+    document.getElementById("post_form").submit();
+} 
