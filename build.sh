f581bc02b8beb55aab62707e585adaa90c80a37e
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-11-20 20:13:52 +0900

    postgresqlの試み

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py create_database
+python manage.py create_database_pgsql
 python manage.py create_superuser
