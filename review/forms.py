e706e34940fbba237ef04be492a1d8843d15f38e
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-12-13 21:06:51 +0900

    投稿時の文字数バリデーションを緩和

--- review/forms.py
+++ review/forms.py
@@ -9,31 +9,31 @@ class ReviewForm(forms.ModelForm):
     # バリデーションエラーがある場合は、エラーを表示する
     def clean_title(self):
         title = self.cleaned_data['title']
-        if len(title) < 3:
+        if len(title) < 2:
             raise forms.ValidationError('タイトルが短すぎます')
         return title
     
     def clean_overall(self):
         overall = self.cleaned_data['overall']
-        if len(overall) < 10:
+        if len(overall) < 3:
             raise forms.ValidationError('総評が短すぎます')
         return overall
 
     def clean_difficulty(self):
         difficulty = self.cleaned_data['difficulty']
-        if len(difficulty) < 10:
+        if len(difficulty) < 3:
             raise forms.ValidationError('課題の難易度についての言及が短すぎます')
         return difficulty
 
     def clean_kadai(self):
         kadai = self.cleaned_data['kadai']
-        if len(kadai) < 10:
+        if len(kadai) < 3:
             raise forms.ValidationError('課題や試験についての言及が短すぎます')
         return kadai
 
     def clean_evaluation(self):
         evaluation = self.cleaned_data['evaluation']
-        if len(evaluation) < 5:
+        if len(evaluation) < 2:
             raise forms.ValidationError('課題の評価についての言及が短すぎます')
         return evaluation
 
